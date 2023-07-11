17c95f4a7dfa535cc25c6b2d0f0707fe1587f1a3
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-07-11 14:39:05 +0900

    Update Select.jsx

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -4,71 +4,60 @@ import { styled } from 'styled-components';
 
 function Select({position}) {
 
-
-    const languageList = ['리액트', '자바', '스프링', '리액트네이티브'];
+  const languageList = ['리액트', '자바', '스프링', '리액트네이티브'];
   const [Language, setLanguage] = useState('리액트');
   const [List, setList] = useState(false);
 
   const node = useRef();
 
-  useEffect(() => {
+  useEffect(() => { // 창의 바깥부분을 클릭하였을때 창이 사라짐
     const clickOutside = (e) => {
-      if (List && node.current && !node.current.contains(e.target)) {
-        setList(false);
-      }
-    };
+      if (List && node.current && !node.current.contains(e.target)) setList(false);};
     document.addEventListener("mousedown", clickOutside);
-    return () => {
-      document.removeEventListener("mousedown", clickOutside);
-    };
+    return () => {document.removeEventListener("mousedown", clickOutside);};
   }, [List]);
 
-  const languageClick = (index) =>{
+  const languageClick = (index) =>{ // 목록을 선택
     setLanguage(languageList[index])
     setList((pre) => !pre)
   }
 
   return (<div ref={node}>
-    <StSelectButton
-      className="flex shrink-0 pointer"
-      onClick={() => setList((pre) => !pre)}
-    >
+    <StSelectButton onClick={() => setList((pre) => !pre)}>
       {Language}<div>▼</div>
     </StSelectButton>
-    {List ? (<StLanguageList position={position}>
+    {List && (<StLanguageList position={position}>  {/* 모달창의 생성방법과 같음 */}
         {languageList.map((item, index)=>{
             return <StLi key={index} onClick={()=>languageClick(index)}>{item}</StLi>
           })}
       </StLanguageList>
-    ) : null}
+    )}
   </div>)
 }
 
 export default Select
 
-
 const StSelectButton = styled.div`
-    width: 300px;
-    height: 40px;
-    padding: 0px 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border: 1px solid rgb(221, 221, 221);
-    background-color: rgb(255, 255, 255);
-    border-radius: 12px;
-    
-    cursor: pointer;
+  width: 300px;
+  height: 40px;
+  padding: 0px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid rgb(221, 221, 221);
+  background-color: rgb(255, 255, 255);
+  border-radius: 12px;
+  cursor: pointer;
 `
 
 const StLanguageList = styled.ul`
-top : 643px; // !
-width: 300px;
-height: 40px;
-margin: 0%;
-padding-left: 0; // ul 밑의 li부분은 기본적으로 padding-left값이 있어 이것을 초기화 해줌
-list-style: none; // 목록 마커 삭제
-position: ${(props) => props.position};
+  top : 643px; // !
+  width: 300px;
+  height: 40px;
+  margin: 0%;
+  padding-left: 0; // ul 밑의 li부분은 기본적으로 padding-left값이 있어 이것을 초기화 해줌
+  list-style: none; // 목록 마커 삭제
+  position: ${(props) => props.position};
 `;
 
 const StLi = styled.li`
